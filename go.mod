02d98d230811b65e05bd3ed561e8793a1b84b69d
Densam2704 <den.samoshckin@yandex.ru>
2021-08-17 01:10:25 +0300

    Took and remade an example from https://github.com/MontFerret/ferret/blob/v0.15.0/examples/embedded/main.go

--- go.mod
+++ go.mod
@@ -2,6 +2,25 @@ module awesomeProject
 
 go 1.17
 
-require github.com/andybalholm/cascadia v1.2.0
+require github.com/MontFerret/ferret v0.15.0
 
-require golang.org/x/net v0.0.0-20180218175443-cbe0f9307d01 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xpath v1.1.11 // indirect
+	github.com/antlr/antlr4 v0.0.0-20200417160354-8c50731894e0 // indirect
+	github.com/corpix/uarand v0.1.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mafredri/cdp v0.31.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.21.0 // indirect
+	github.com/sethgrid/pester v1.1.0 // indirect
+	github.com/wI2L/jettison v0.7.1 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
--- site parser.go
+++ site parser.go
@@ -1,69 +1,81 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"golang.org/x/net/html"
-	_ "golang.org/x/net/html"
-	"io"
-	"log"
-	"net/http"
-	"strings"
+	"os"
+
+	"github.com/MontFerret/ferret/pkg/compiler"
+	"github.com/MontFerret/ferret/pkg/drivers"
+	"github.com/MontFerret/ferret/pkg/drivers/cdp"
+	"github.com/MontFerret/ferret/pkg/drivers/http"
 )
 
-type site struct {
-	name string
-	url  string
+type Topic struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 func main() {
-	var s site
-	s.url = "https://www.citilink.ru/search/?text=смартфоны"
-	s.name = "citilink"
+	topics, err := getTopTenSmartphones()
 
-	fmt.Printf("Это программа парсер, которая парсит сайт \"%s\" и выводит все смартфоны\n", s.url)
-
-	response, err := http.Get(s.url)
-
-	//if there was an error, report it and exit
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	defer response.Body.Close()
-
-	////check response status code
-	//if response.StatusCode != http.StatusOK {
-	//	log.Fatalf("Response status code was %d\n", response.StatusCode)
-	//}
-
-	//check response content type
-	contentType := response.Header.Get("Content-Type")
-	if !strings.HasPrefix(contentType, "text/html"){
-		log.Fatalf("Response content type was %s (not text/html)\n",contentType)
+	for _, topic := range topics {
+		fmt.Println(fmt.Sprintf("%s \tURL: %s", topic.Name, topic.URL))
 	}
+}
 
-	//loop until we find the title element and its content
-	//or encounter an error (which includes the end of the stream)
-	htmlTokens := html.NewTokenizer(response.Body)
-	for {
-		tokenType := htmlTokens.Next()
-		//if it's an error token, we either reached
-		//the end of the file, or the HTML was malformed
-		if tokenType==html.ErrorToken{
-			err:=htmlTokens.Err()
-			if err==io.EOF{
-				break
+func getTopTenSmartphones() ([]*Topic, error) {
+	query := `
+		LET doc = DOCUMENT("https://www.citilink.ru/catalog/smartfony")
+
+		FOR el IN ELEMENTS(doc, ".ProductCardHorizontal__header-block")
+			LIMIT 10
+			LET url = ELEMENT(el, "a")
+			LET name = ELEMENT(el, "a")
+			LET price = ELEMENT(el,".ProductCardHorizontal__price_current-price")
+		
+			RETURN{
+				name: TRIM(name.attributes.title),
+				url: "https://www.citilink.ru" + url.attributes.href
 			}
-			log.Fatalf("error tokenizing html: %v",htmlTokens.Err())
-		}
-		//Todo figure out how to get product fields
-		////the token has text content withing tag
-		//if tokenType==html.TextToken{
-		//	token:=htmlTokens.Token()
-		//	if strings.HasPrefix(token.Data,"<div class=\"product_data__gtm-js product_data__pageevents-js"){
-		//		fmt.Println(token.Data)
-		//	}
-		//}
+	`
+	comp := compiler.New()
+
+	program, err := comp.Compile(query)
+
+	if err != nil {
+		return nil, err
 	}
 
+	// create a root context
+	ctx := context.Background()
+
+	// enable HTML drivers
+	// by default, Ferret Runtime does not know about any HTML drivers
+	// all HTML manipulations are done via functions from standard library
+	// that assume that at least one driver is available
+	ctx = drivers.WithContext(ctx, cdp.NewDriver())
+	ctx = drivers.WithContext(ctx, http.NewDriver(), drivers.AsDefault())
+
+	out, err := program.Run(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]*Topic, 0, 10)
+
+	err = json.Unmarshal(out, &res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
